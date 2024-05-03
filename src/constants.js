cb3f7763bea5fd5b1be31227688d5ea01874c57a
matiaslascano <matias_lascano@hotmail.com>
2024-05-03 14:49:49 -0300

    First attemp of home

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+const iconSocial = [
+    {
+        id: 1,
+        name: "Facebook",
+        icon: "assets/icons/Icon_facebook.png",
+        url: "https://www.facebook.com/",
+    },
+    {
+        id: 1,
+        name: "Instagram",
+        icon: "assets/icons/Icon_instagram.png",
+        url: "https://www.instagram.com/",
+    },
+    {
+        id: 1,
+        name: "Twitter",
+        icon: "assets/icons/Icon_twitter.png",
+        url: "https://www.twitter.com/",
+    },
+    {
+        id: 1,
+        name: "YouTube",
+        icon: "assets/icons/Icon_youtube.png",
+        url: "https://www.youtube.com/",
+    }
+]
+
+export default iconSocial
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				verde1: '#233000',
+				verde2: '#5E6600',
+				verde3: '#9CAA00',
+				verde4: '#EBF0E4',
+				gris1: '#4D4D4D',
+				gris2: '#B0B0B0',
+			},
+			fontFamily: {
+				rufina: ['Rufina', 'serif'],
+				lato: ['Lato', 'sans-serif'],
+			}
+		},
 	},
 	plugins: [],
 }
