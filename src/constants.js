794352d6cc89ac71ad7466dea86a9f8249922ae5
matiaslascano <matias_lascano@hotmail.com>
2024-05-04 17:08:28 -0300

    Home frontend ready

--- src/constants.js
+++ src/constants.js
@@ -6,23 +6,132 @@ const iconSocial = [
         url: "https://www.facebook.com/",
     },
     {
-        id: 1,
+        id: 2,
         name: "Instagram",
         icon: "assets/icons/Icon_instagram.png",
         url: "https://www.instagram.com/",
     },
     {
-        id: 1,
+        id: 3,
         name: "Twitter",
         icon: "assets/icons/Icon_twitter.png",
         url: "https://www.twitter.com/",
     },
     {
-        id: 1,
+        id: 4,
         name: "YouTube",
         icon: "assets/icons/Icon_youtube.png",
         url: "https://www.youtube.com/",
-    }
+    },
+];
+
+const menu = [
+    {
+        id: 1,
+        name: "Filete de bacalao blanco",
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+        price: '20'
+    },
+    {
+        id: 2,
+        name: "Filete con mantequilla de romero",
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+        price: '22'
+    },
+    {
+        id: 3,
+        name: "Ensalada de pepino",
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+        price: '18'
+    },
+    {
+        id: 4,
+        name: "Maridaje natural",
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+        price: '90'
+    },
+];
+
+const features = [
+    {
+        id: 1,
+        name: "Calidad Premium",
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+        icon: "assets/vectors/Icon_fish.svg"
+    },
+    {
+        id: 2,
+        name: "Productos de estación",
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+        icon: "assets/vectors/Icon_carrot.svg"
+    },
+    {
+        id: 3,
+        name: "Vegetales y frutas frescas",
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+        icon: "assets/vectors/Icon_lemon.svg"
+    },
+];
+
+const blogs = [
+    {
+        id: 1,
+        image: "assets/images/blog-image-1.webp",
+        avatar: "assets/images/Avatar1.png",
+        name: "Julie Christie",
+        day: "October 17,2021",
+        hour: "3:33 pm",
+        comments: "2 comments",
+        title: "Frutas y hortalizas y protección contra las enfermedades",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+    },
+    {
+        id: 2,
+        image: "assets/images/blog-image-2.webp",
+        avatar: "assets/images/Avatar2.png",
+        name: "Dianne Russell",
+        day: "October 17,2021",
+        hour: "3:33 pm",
+        comments: "2 comments",
+        title: "Ensalada de primavera de espárragos con rúcula y queso de cabra",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+    },
+];
+
+const categories = [
+    {
+        id: 1,
+        name: "Entradas",
+        image: "assets/images/Category1.webp",
+        arrow: "assets/icons/Icon_arrow-right.svg"
+    },
+    {
+        id: 2,
+        name: "Principales",
+        image: "assets/images/Category2.webp",
+        arrow: "assets/icons/Icon_arrow-right.svg"
+    },
+    {
+        id: 3,
+        name: "Sopas",
+        image: "assets/images/Category3.webp",
+        arrow: "assets/icons/Icon_arrow-right.svg"
+    },
+];
+
+const testimonials = [
+    {
+        id: 1,
+        name: "Laura Quinteros",
+        avatar: "/assets/images/Avatar3.png",
+        testimonial: "“ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus  Lorem ipsum dolor sit amet. ",
+    },
+    {
+        id: 2,
+        name: "Matias Lascano",
+        avatar: "/assets/images/Avatar1.png",
+        testimonial: "“ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem id penatibus imperdiet. Turpis egestas ultricies purus  Lorem ipsum dolor sit amet. ",
+    },
 ]
 
-export default iconSocial
+export { iconSocial, menu, features, blogs, categories, testimonials }
